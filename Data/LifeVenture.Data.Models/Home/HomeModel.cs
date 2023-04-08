02ed269413fe3599f3da6572c9955df4bb6ed254
DarkkIT <50321264+DarkkIT@users.noreply.github.com>
2023-04-08 18:23:59 +0300

    Add statictics

--- Data/LifeVenture.Data.Models/Home/HomeModel.cs
+++ Data/LifeVenture.Data.Models/Home/HomeModel.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LifeVenture.Data.Models.Home
+﻿namespace LifeVenture.Data.Models.Home
 {
-    internal class HomeModel
+    using System.Collections.Generic;
+
+    using LifeVenture.Data.Common.Models;
+    using LifeVenture.Data.Models.Common;
+
+    public class HomeModel
     {
     }
 }
