06dfdbee6577bff9df7500ae1f81a526ad8d60c2
DarkkIT <50321264+DarkkIT@users.noreply.github.com>
2023-11-06 14:44:31 +0200

    Add About Us page

---
+++ Web/LifeVenture.Web.ViewModels/AboutUs/AboutUsViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace LifeVenture.Web.ViewModels.AboutUs
+{
+    using LifeVenture.Web.ViewModels.Common;
+
+    public class AboutUsViewModel
+    {
+        public MessageInputModel Input { get; set; }
+    }
+}
---
+++ Web/LifeVenture.Web.ViewModels/Common/MessageViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace LifeVenture.Web.ViewModels.Common
+{
+    using LifeVenture.Data.Models.Common;
+    using LifeVenture.Services.Mapping;
+
+    public class MessageViewModel : IMapFrom<Message>
+    {
+        public int Id { get; set; }
+
+        public string SenderName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Subject { get; set; }
+
+        public string Description { get; set; }
+
+        public int? Phone { get; set; }
+    }
+}
