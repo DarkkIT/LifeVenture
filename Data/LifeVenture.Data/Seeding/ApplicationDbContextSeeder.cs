f57f993e9acd2e35d1ce492d7090735656cacd05
EmilVuchev <emo_vuchev_90@abv.bg>
2023-11-09 20:18:19 +0200

    Add phone codes and change country phone code model

--- Data/LifeVenture.Data/Seeding/ApplicationDbContextSeeder.cs
+++ Data/LifeVenture.Data/Seeding/ApplicationDbContextSeeder.cs
@@ -27,6 +27,7 @@
                           {
                               new RolesSeeder(),
                               new SettingsSeeder(),
+                              new CountryPhoneCodesSeeder(),
                           };
 
             foreach (var seeder in seeders)
