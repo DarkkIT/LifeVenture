0809910b54211d6ab2b159ec8d381dd9f9f7c8b2
EmilVuchev <emo_vuchev_90@abv.bg>
2023-12-16 18:54:06 +0200

    Seed locations in Bulgaria

---
+++ Data/LifeVenture.Data.Models/Locations/Region.cs
@@ -0,0 +1,21 @@
+﻿namespace LifeVenture.Data.Models.Locations
+{
+    using System.Collections.Generic;
+
+    using LifeVenture.Data.Common.Models;
+    using LifeVenture.Data.Models.Locations.Interfaces;
+
+    public class Region : BaseDeletableModel<int>, ILocation
+    {
+        public Region()
+        {
+            this.Municipalities = new HashSet<Municipality>();
+        }
+
+        public string Name { get; set; }
+
+        public string Code { get; set; }
+
+        public ICollection<Municipality> Municipalities { get; set; }
+    }
+}
