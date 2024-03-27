4b88c4db7411e15b88fad71f200b3ca930c56acc
DarkkIT <50321264+DarkkIT@users.noreply.github.com>
2024-03-27 22:25:30 +0200

    Update login

--- Web/LifeVenture.Web/Program.cs
+++ Web/LifeVenture.Web/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace LifeVenture.Web
+namespace LifeVenture.Web
 {
     using System.Reflection;
 
@@ -29,6 +29,11 @@
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            //var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
+
+            //builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
+            //builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
             ConfigureServices(builder.Services, builder.Configuration);
             var app = builder.Build();
             Configure(app);
